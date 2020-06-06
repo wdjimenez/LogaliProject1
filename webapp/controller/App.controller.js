8ff6deaba99b553ec399effbc186ed4dcc27202b
wjimenez <jiman1983mx@gmail.com>
2020-06-06 04:29:13 +0000

    Se agrega la parte de internacionalización para los textos, dependiendo del archivo de configuración y del idioma de la navegación es como se toma el texto correspondiente: {i18n>showHelloButtonText} i18n es el nombre que se define en el controlador

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -2,15 +2,23 @@ sap.ui.define(
 	[
 		"sap/ui/core/mvc/Controller",
 		"sap/m/MessageToast",
-		"logali/SAPUI5/model/models"
+		"logali/SAPUI5/model/models",
+		"sap/ui/model/resource/ResourceModel"
 	],
-	function (Controller, MessageToast, models) {
+	function (Controller, MessageToast, models, ResourceModel) {
 		"use strict";
 		return Controller.extend(
 			"logali.SAPUI5.controller.App", {
 				onInit: function(){
 					//Set model on view
 					this.getView().setModel(models.createRecipient());
+					
+					//set i18n model on view
+					var i18nModel = new ResourceModel({
+						bundleName: "logali.SAPUI5.i18n.i18n"
+					}); 
+					
+					this.getView().setModel(i18nModel, "i18nn");
 				},
 
 				onShowHello: function () {
